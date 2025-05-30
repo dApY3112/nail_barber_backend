784d6a6b981ae0c71051fa5617de10fd95a1d5e9
dApY3112 <98270020+dApY3112@users.noreply.github.com>
2025-05-30 21:47:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from app.api.api_v1.router import api_router
 from app.db.session import engine
 from app.models.base import Base
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from sqlalchemy import create_engine
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL, echo=True)
 
 app = FastAPI(title="Nail&Barber API")
 app.add_middleware(
